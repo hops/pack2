cea92c301bd250bc4bb6c509a41e6d8e43f9ab14
hops <dev@0x23.ch>
2020-05-15 23:53:07 +0200

    more descriptive help

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ mod filtermask;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "pack2")]
 pub enum CmdOpts {
+    /// Generates statistics from a [input] and writes masks to <output>
+    /// stats are written to stderr
     #[structopt(name = "statsgen")]
     Statsgen {
         /// Input file, stdin if not present
@@ -19,13 +21,15 @@ pub enum CmdOpts {
         /// Separator used in mask output [default: TAB]
         #[structopt(short, long, display_order=2)]
         separator: Option<char>,
-        /// Ignore passwords shorter than min-length
+        /// Ignore passwords shorter than <min-length>
         #[structopt(long, default_value="1", display_order=13)]
         min_length: u16,
-        /// Ignore passwords longer than max-length
+        /// Ignore passwords longer than <max-length>
         #[structopt(long, default_value="65535", display_order=14)]
         max_length: u16,
     },
+
+    /// Decodes $HEX[] encoded lines
     #[structopt(name = "unhex")]
     Unhex {
         /// Input file, stdin if not present
@@ -35,6 +39,7 @@ pub enum CmdOpts {
         #[structopt(short, long, parse(from_os_str))]
         output: Option<PathBuf>,
     },
+    /// Filters a wordlist by a given mask
     #[structopt(name = "filtermask")]
     Filtermask {
         /// Mask to filter by
